c9ffc5d23dca81698a6d3d93b62a5d6545afc92d
DuckAfire <155199080+duckafire@users.noreply.github.com>
2025-12-05 15:51:19 -0300

    Add type validator (to use during the runtime)

---
+++ src/LazyLoadingImage.ts
@@ -0,0 +1,93 @@
+/*
+ * Zlib License
+ *
+ * Copyright (C) 2025 DuckAfire <duckafire.github.io/nest>
+ *
+ * This software is provided 'as-is', without any express or implied
+ * warranty. In no event will the authors be held liable for any damages
+ * arising from the use of this software.
+ *
+ * Permission is granted to anyone to use this software for any purpose,
+ * including commercial applications, and to alter it and redistribute it
+ * freely, subject to the following restrictions:
+ *
+ * 1. The origin of this software must not be misrepresented; you must not
+ *    claim that you wrote the original software. If you use this software
+ *    in a product, an acknowledgment in the product documentation would be
+ *    appreciated but is not required.
+ * 2. Altered source versions must be plainly marked as such, and must not be
+ *    misrepresented as being the original software.
+ * 3. This notice may not be removed or altered from any source distribution.
+ * */
+
+class __TypeValidator__
+{
+	#value: unknown = undefined;
+	#fallbackValue: unknown = undefined;
+	#exceptionMessage: string = "A type error occur.";
+	#validValue: boolean = false;
+	#safe: boolean = false;
+
+	#expectStuff(condit: boolean, exMessage: string): __TypeValidator__
+	{
+		this.#validValue = condit;
+		this.#exceptionMessage = exMessage;
+		return this;
+	}
+
+	constructor(v: unknown)
+	{
+		this.#value = v;
+	}
+
+	expectType(t: string): __TypeValidator__
+	{
+		const TYPE = typeof this.#value;
+
+		return this.#expectStuff(
+			(TYPE !== t),
+			`Expecting type "${t}", instead "${TYPE}".`,
+		);
+	}
+
+	expectTag(): __TypeValidator__
+	{
+		return this.#expectStuff(
+			(this.#value instanceof HTMLElement),
+			`Expecting instance of "HTMLElement", instead "${(this.#value as Function).constructor.name}".`,
+		);
+	}
+
+	fallback(v: unknown): __TypeValidator__
+	{
+		// It, indirectly, enables
+		// "safe mode".
+		this.#fallbackValue = v;
+		return this;
+	}
+
+	noException(): __TypeValidator__
+	{
+		this.#safe = true;
+		return this;
+	}
+
+	validate(): unknown
+	{
+		if(this.#validValue)
+			return this.#value;
+
+		if(this.#fallbackValue !== undefined)
+			return this.#fallbackValue;
+
+		if(!this.#safe)
+			throw new TypeError( this.#exceptionMessage );
+
+		return null;
+	}
+	
+	onlyValidate(): boolean
+	{
+		return this.#validValue
+	}
+};
